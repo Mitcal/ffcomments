e36d9b07e0598b7993575d360232a5d723ecce83
WebMaster VPS1 <webmaster@fuelfixeruk.co.uk>
2020-12-18 12:08:16 +0000

    db fix

--- app/Http/Controllers/Frontend/CommentController.php
+++ app/Http/Controllers/Frontend/CommentController.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers\Frontend;
 use App\ContentPage;
 use App\Comment;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Auth;
 class CommentController extends Controller
 {
     public function save(Request $request)
     {	
+        Validator::extend('mustBeEmpty', function($attr, $value, $params){
+            if(!empty($attr)) return false;
+            return true;
+        });
+        
         $request->validate([
             'content' => 'required|string',
             'author_name' => 'required|string|max:255',
             'author_email' => 'required|email',
+            'website' => 'mustBeEmpty',
         ]);
 
         $comment = new Comment($request->only(['content', 'author_name', 'author_email', 'page_id', 'parent_id']));
